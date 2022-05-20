275881d2661715e349918e1369d1df467b7ab96f
Lucas Roberto de Oliveira Barcelos <lucasolbarcelos@gmail.com>
2022-05-20 16:20:51 -0300

    @LucassRoberto Task: Criação do projeto

---
+++ src/main/java/com/projeto/apimarvel/ApimarvelApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.apimarvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimarvelApplication.class, args);
+	}
+
+}
